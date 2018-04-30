3e15ee897383378e1f5ec11eb92cedb0abb8f404
David Tanzer <business@davidtanzer.net>
2018-04-30 06:45:53 +0200

    Reducer split into two with combineReducers

--- src/Reducer.js
+++ src/Reducer.js
@@ -1,24 +1,33 @@
 import { fromJS } from 'immutable';
+import { combineReducers } from 'redux-immutable';
 
-const initialState = fromJS({
-	squares: [
-		{ x: 316, y: 281, },
-		{ x: 632, y: 437, },
-	],
-	edges: [
-		{ x: 0,    y: 0,   squareX: 0,  squareY: 0,  },
-		{ x: 1024, y: 0,   squareX: 50, squareY: 0,  },
-		{ x: 0,    y: 768, squareX: 0,  squareY: 50, },
-		{ x: 1024, y: 768, squareX: 50, squareY: 50, },
-	],
+export const reducer = combineReducers({
+	squares: squaresReducer,
+	edges: edgesReducer,
 });
 
-export function reducer(state = initialState, action) {
+const edgesInitialState = fromJS([
+	{ x: 0,    y: 0,   squareX: 0,  squareY: 0,  },
+	{ x: 1024, y: 0,   squareX: 50, squareY: 0,  },
+	{ x: 0,    y: 768, squareX: 0,  squareY: 50, },
+	{ x: 1024, y: 768, squareX: 50, squareY: 50, },
+]);
+
+function edgesReducer(state = edgesInitialState, action) {
+	return state
+}
+
+const squaresInitialState = fromJS([
+	{ x: 316, y: 281, },
+	{ x: 632, y: 437, },
+]);
+
+function squaresReducer(state = squaresInitialState, action) {
 	switch(action.type) {
 		case 'SQUARE_MOVED':
 			return state
-				.updateIn(['squares', action.id, 'x'], x => x+action.dx)
-				.updateIn(['squares', action.id, 'y'], y => y+action.dy);
+				.updateIn([action.id, 'x'], x => x+action.dx)
+				.updateIn([action.id, 'y'], y => y+action.dy);
 		}
 	return state;
 }
